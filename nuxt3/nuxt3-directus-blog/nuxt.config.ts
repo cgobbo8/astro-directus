24e36bb4142ef45e19be96d1fac1534359ef535b
Corentin Gobbo <corentin.gobbo@hotmail.fr>
2024-05-30 22:15:27 +0200

    delete - delete other examples

--- nuxt3/nuxt3-directus-blog/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-export default defineNuxtConfig({
-	modules: ["nuxt-directus", "@nuxtjs/tailwindcss"],
-	directus: {
-		url: "-",
-		token: "-"
-	},
-});
--- website-nuxt3/plugins/directus.js
+++
@@ -1,9 +0,0 @@
-import { createDirectus } from '@directus/sdk'
-import { rest, readItem, readItems } from '@directus/sdk/rest'
-const directus = createDirectus('https://CHANGE-THIS.directus.app').with(rest())
-
-export default defineNuxtPlugin(() => {
-	return {
-		provide: { directus, readItem, readItems },
-	};
-});
